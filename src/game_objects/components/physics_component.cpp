cf292affd008142f749e6e82fe91f8d5e905c6a5
TheMrCake <antonlichota@gmail.com>
2025-12-03 16:05:03 +0000

    Change collision component to physics component

---
+++ src/game_objects/components/physics_component.cpp
@@ -0,0 +1,18 @@
+#include "game_objects/components/physics_component.hpp"
+
+// Local includes
+#include "game_objects/entity.hpp"
+
+
+
+Physics::Physics()
+  : Component<Physics>(INVALID_ENTITY_ID)
+  {}
+
+Physics::Physics(EntityId entity_id)
+  : Component<Physics>(entity_id)
+  {}
+
+void Collision::update_impl(const float delta_time) {
+  
+}
