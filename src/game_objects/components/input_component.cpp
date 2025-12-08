7b0f128e71ffbff7cec4d1d20e4de3e68d554af1
TheMrCake <antonlichota@gmail.com>
2025-12-08 08:18:46 +0000

    Initialise unintialised data

--- src/game_objects/components/input_component.cpp
+++ src/game_objects/components/input_component.cpp
@@ -4,7 +4,11 @@
 #include "game_objects/entity.hpp"
 
 Input::Input()
-    : Component<Input>(INVALID_ENTITY_ID) {}
+    : Component<Input>(INVALID_ENTITY_ID),
+      input_direction(),
+      possess_pressed() {}
 
 Input::Input(EntityId entity_id)
-    : Component<Input>(entity_id) {}
+    : Component<Input>(entity_id),
+      input_direction(),
+      possess_pressed() {}
